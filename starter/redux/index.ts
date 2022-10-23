cfd3619aef7c4b90721326f375b42839f1b211b1
Anthony Chan <anthony.chan@synpulse.com>
2022-10-23 16:54:44 +0800

    fix starter path

---
+++ starter/redux/index.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import logger from 'redux-logger'
+import { exampleSlice } from '../slice/example'
+import { persistReducer, persistStore } from 'redux-persist'
+import {
+  Reducer,
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
+
+
+const persistConfig = (blacklist?: string[]) => ({
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist,
+})
+
+const persistedReducer = (reducers: { [key: string]: Reducer }, blacklist?: string[]) => persistReducer(persistConfig(blacklist), combineReducers({ ...reducers, example: exampleSlice.reducer }))
+
+export default (reducers: { [key: string]: Reducer }, blacklist?: string[]) => {
+  const store = configureStore({
+    reducer: persistedReducer(reducers, blacklist),
+    middleware: [...getDefaultMiddleware({ serializableCheck: false }), logger],
+  })
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
